b3ec9bb9f6d4e2b50433acb4209a9f589d6fecf5
ikhfhm <ikhwanfahmihidayat@gmail.com>
2021-01-17 10:17:16 +0000

    #10 - Override url register to login

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,8 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::match(['get', 'post'], '/register', function() {
+    return redirect('/login');
+})->name('register');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
