d6d5cf71d0ad36f6506f3189af1805259ea8990b
ikhfhm <ikhwanfahmihidayat@gmail.com>
2021-01-18 07:56:17 +0000

    #40 - Add route resource for category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -27,3 +28,4 @@ Route::match(['get', 'post'], '/register', function() {
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::resource('users', UserController::class);
+Route::resource('categories', CategoryController::class);
