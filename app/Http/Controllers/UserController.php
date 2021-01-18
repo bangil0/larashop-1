3c1572ce950a0b05e7c60d872a7ee5eb56cd0132
ikhfhm <ikhwanfahmihidayat@gmail.com>
2021-01-18 03:01:21 +0000

    #29 - Create users detail page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -62,7 +62,7 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        $user = User::findOrdFail($id);
+        $user = User::findOrFail($id);
         return view('users.show', ['user' => $user]);
     }
 
