cf5bb721e316c83aa873cdb4ee9edb580d7ab2c3
ikhfhm <ikhwanfahmihidayat@gmail.com>
2021-01-19 11:52:38 +0000

    #109 - Show error messages in form create category

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -14,15 +14,19 @@
         <form enctype="multipart/form-data" class="bg-white shadow-sm p-3" action="{{route('categories.store')}}" method="POST">
             @csrf
             <label>Category name</label><br>
-            <input type="text" class="form-control" name="name"/>
+            <input type="text" class="form-control {{$errors->first('name') ? "is-invalid" : ""}}" value="{{old('name')}}" name="name">
+            <div class="invalid-feedback">
+                {{$errors->first('name')}}
+            </div>
             <br>
+
             <label>Category image</label>
-            <input type="file" class="form-control" name="image"/>
-
+            <input type="file" class="form-control {{$errors->first('image') ? "is-invalid" : ""}}" name="image">
+            <div class="invalid-feedback">
+                {{$errors->first('image')}}
+            </div>
             <br>
-
-            <input type="submit" class="btn btn-primary" value="Save"/>
-
+            <input type="submit" class="btn btn-primary" value="Save">
         </form>
     </div>
 @endsection
