c6364e17c7030fc3e15ffd4938c3a4a1a6c87b88
ikhfhm <ikhwanfahmihidayat@gmail.com>
2021-01-19 12:06:44 +0000

    #112 - Create valide for create book data in controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -44,6 +44,16 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
+        \Validator::make($request->all(), [
+            "title" => "required|min:3|max:200",
+            "description" => "required|min:20|max:1000",
+            "author" => "required|min:3|max:100",
+            "publisher" => "required|min:3|max:200",
+            "price" => "required|digits_between:0,10",
+            "stock" => "required|digits_between:0,10",
+            "cover" => "required"
+        ])->validate();
+        
         $new_book = new Book;
 
         $new_book->title = $request->get('title');
