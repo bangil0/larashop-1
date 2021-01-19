e441a920a4f38539ec0af1a81b7fb224a41629fe
ikhfhm <ikhwanfahmihidayat@gmail.com>
2021-01-19 04:43:29 +0000

    #75 - Import softdeletes in book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function categories() {
         return $this->belongsToMany(Category::class);
