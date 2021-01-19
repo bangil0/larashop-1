8f252555341e27c76f78e21c384495432e03e687
ikhfhm <ikhwanfahmihidayat@gmail.com>
2021-01-19 09:41:34 +0000

    #97 - Make dynamic property in order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -16,4 +16,13 @@ class Order extends Model
     public function books() {
         return $this->belongsToMany(Book::class)->withPivot('quantity');
     }
+
+    public function getTotalQuantityAttribute() {
+        $total_quantity = 0;
+        foreach($this->books as $book) {
+            $total_quantity += $book->pivot->quantity;
+        }
+
+        return $total_quantity;
+    }
 }
