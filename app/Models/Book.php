b995e132145cc6cdf29104d68b3802bdfb0cdc4b
ikhfhm <ikhwanfahmihidayat@gmail.com>
2021-01-19 01:34:47 +0000

    #62 - Declare many to many relation in category and book model

---
+++ app/Models/Book.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    public function categories() {
+        return $this->belongsToMany('Category');
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Book;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,4 +11,8 @@ class Category extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function books() {
+        return $this->belongsToMany('Books');
+    }
 }
